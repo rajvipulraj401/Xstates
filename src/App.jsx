936acd2db9f366228a3115403d2cb3f5d1800b9f
vipul <rajvipulraj401@gmail.com>
2025-04-03 10:16:17 +0530

    completed this project

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,157 @@
 import React, { useEffect, useState } from "react";
-import Cards from "./components/Cards";
 import "./App.css";
 
 const App = () => {
-  const [data, setData] = useState([]);
+  let [country, setCountry] = useState([]); // we will get country's array
+  let [state, setState] = useState([]); // we will get state's array
+  let [city, setCity] = useState([]); // we will get city's array
+  const [selectedCountry, setSelectedCountry] = useState("");
+  const [selectedState, setSelectedState] = useState("");
+  const [selectedCity, setSelectedCity] = useState("");
+
   useEffect(() => {
-    fetch(`https://xcountries-backend.azurewebsites.net/all`)
-      .then((res) => res.json())
-      .then((data) => setData(data))
-      .catch((err) => console.error(`Error fetching data:${err}`));
+    fetchCountry("https://crio-location-selector.onrender.com/countries");
   }, []);
+  // On mount only i want that we have all the country data with us
+
+  useEffect(() => {
+    fetchState(
+      `https://crio-location-selector.onrender.com/country=${selectedCountry}/states`
+    );
+  }, [selectedCountry]);
+
+  // Step2:-- When we select the country then only make this api call to
+  // get the data of state.
+
+  useEffect(() => {
+    fetchCity(
+      `https://crio-location-selector.onrender.com/country=${selectedCountry}/state=${selectedState}/cities`
+    );
+  }, [selectedState]);
+
+  const fetchCountry = async (url) => {
+    try {
+      const res = await fetch(url);
+
+      const response = await res.json();
+      console.log(response);
+      setCountry(response);
+    } catch (err) {
+      console.error("Error fetching countries:", err);
+      return [];
+    }
+  };
+  const fetchState = async (url) => {
+    try {
+      const res = await fetch(url);
+
+      const response = await res.json();
+      console.log(response);
+      setState(response);
+    } catch (err) {
+      console.error("Error fetching countries:", err);
+      return [];
+    }
+  };
+  const fetchCity = async (url) => {
+    try {
+      const res = await fetch(url);
+
+      const response = await res.json();
+      console.log(response);
+      setCity(response);
+    } catch (err) {
+      console.error("Error fetching countries:", err);
+      return [];
+    }
+  };
 
   return (
     <>
-      <div className="card-container">
-        {data.map((curr) => {
-          console.log(curr.name); // ✅ This will log names before rendering
-          return (
-            <Cards
-              key={curr.name}
-              item={{ name: curr.name, flag: curr.flag }}
-            />
-          );
-        })}
-        {/* <Cards data={data}></Cards> */}
+      <div className="container">
+        <h1>Select Location</h1>
+
+        <div className="select-container">
+          {/*--------------------- Select1 (Country)--------------------------- */}
+          <select
+            name="Countries"
+            className="country-select select"
+            onChange={(event) => {
+              setSelectedCountry(event.target.value);
+            }}
+          >
+            {/* Jab tu koi option choose karega, toh poora 
+            <select> element ka value change hoga, aur us change ko 
+            track karne ke liye `onChange` event diya gaya hai. */}
+            <option value="" className="placeholder" hidden>
+              Select Country
+            </option>
+            {/* <option value="india">India</option>
+            <option value="usa">USA</option> */}
+            {country.length > 0 &&
+              country.map((option) => {
+                return (
+                  <option key={option} value={option}>
+                    {option}
+                  </option>
+                );
+              })}
+            {/* Map country data and fill the option values from the API */}
+          </select>
+
+          {/*---------------------- Select2 (State)------------------------------ */}
+          <select
+            name="state"
+            className="state-select select"
+            disabled={selectedCountry === ""}
+            onChange={(event) => {
+              setSelectedState(event.target.value);
+            }}
+          >
+            <option value="" className="placeholder" hidden>
+              {/* By putting hidden here this option will get once we select any other option*/}
+              Select State
+            </option>
+            {state.length > 0 &&
+              state.map((option) => {
+                return (
+                  <option key={option} value={option}>
+                    {option}
+                  </option>
+                );
+              })}
+
+            {/* Map and fill the option values from the API */}
+          </select>
+
+          {/*------------------ Select3 (City) ------------------------------------*/}
+          <select
+            name="city"
+            className="city-select select"
+            disabled={selectedState === ""}
+            onChange={(event) => {
+              setSelectedCity(event.target.value);
+              // ----value attribute was passed in Option tag inside the select tag so when one option se selected we can chech which event
+              // was targetted using event.target.value-----
+            }}
+          >
+            <option value="" className="placeholder" hidden>
+              Select City
+            </option>
+            {/* <option value="mumbai">Mumbai</option>
+            <option value="newyork">New York</option> */}
+
+            {city.length > 0 &&
+              city.map((option) => {
+                return (
+                  <option key={option} value={option}>
+                    {option}
+                  </option>
+                );
+              })}
+            {/* Map and fill the option values from the API of  */}
+          </select>
+        </div>
       </div>
     </>
   );
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,13 +1,14 @@
 import demoImg from "../assets/billy.png";
 import React from "react";
-const Cards = ({ item }) => {
+// const Cards = ({ item }) => {
+const Cards = () => {
   //We are getting an object inside our props object {{} } so we destructure it
   return (
     <div className="cards">
-      <img src={item.flag} alt="name" />
-      <p>{item.name}</p>
-      {/* <img src={demoImg} alt="demoImg" />
-      <p>Billy Idol </p> */}
+      {/* <img src={item.flag} alt="name" />
+      <p>{item.name}</p> */}
+      <img src={demoImg} alt="demoImg" />
+      <p>Billy Idol </p>
     </div>
   );
 };
